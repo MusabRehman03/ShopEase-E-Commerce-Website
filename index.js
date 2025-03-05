ca13d77486e7ff6f7aed724bc0a4bcaf408c852a
Musab Rehman <rehmanmusab0302@gmail.com>
2025-03-05 15:16:32 +0500

    fix: remove cartCountArray and added count in each object of cart

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 let arr = [];
-let cartCountArray = JSON.parse(localStorage.getItem("cartCountArray")) || [
-  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-  0, 0, 0, 0,
-];
-let totalNumberOfProductsInCart = 0;
+// let cartCountArray = JSON.parse(localStorage.getItem("cartCountArray")) || [
+//   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//   0, 0, 0, 0,
+// ];
+let totalNumberOfProductsInCart=0
 let total = 0;
 let currentPage = 1;
 let cartArray = JSON.parse(localStorage.getItem("cartArray")) || [];
@@ -18,9 +18,9 @@ async function fetchData() {
   let raw = await fetch("https://dummyjson.com/product");
   let data = await raw.json();
   arr = data.products;
-  //   console.log(arr);
+//   console.log(arr);
   let paginatedArray = arr.slice(skip, skip + limit);
-  //   console.log(paginatedArray);
+//   console.log(paginatedArray);
   let pages = document.getElementById("pages");
   pages.classList.remove("hidden");
   displayData(paginatedArray);
@@ -45,25 +45,25 @@ function displayData(array = arr) {
   let buttons = div.querySelectorAll(".addToCart");
   buttons.forEach((button) => {
     button.addEventListener("click", () => {
-      //   console.log("data=>", array[button.getAttribute("index")]);
-      const temp = array[button.getAttribute("index")];
-      temp.count = 1;
-      cartCountArray[button.getAttribute("index")]++;
-      localStorage.setItem("cartCountArray", JSON.stringify(cartCountArray));
+    //   cartCountArray[button.getAttribute("index")]++;
+    //   localStorage.setItem("cartCountArray", JSON.stringify(cartCountArray));
 
       let c = 0;
       // console.log('loopstart')
       // console.log(Number( button.getAttribute("index"))+1)
       for (let i = 0; i < cartArray.length; i++) {
         if (Number(button.getAttribute("index")) + 1 == cartArray[i].id) {
-          //   console.log(cartArray[i]);
-          //   console.log("present");
+            cartArray[i].count++
           c++;
+
         }
       }
       // console.log('loopEnd')
       if (c == 0) {
         // console.log("if");
+        const temp = array[button.getAttribute("index")]
+        // console.log(temp)
+        temp.count=1
         cartArray.push(array[button.getAttribute("index")]);
         localStorage.setItem("cartArray", JSON.stringify(cartArray));
       }
@@ -114,34 +114,36 @@ function cart() {
 fetchData();
 displayData();
 function displayCartData(array = cartArray) {
-  //   console.log(cartCountArray);
+//   console.log(cartCountArray);
   document.getElementById("total").classList.remove("hidden");
   div.innerHTML = "";
   total = 0;
-  totalNumberOfProductsInCart = 0;
+  totalNumberOfProductsInCart=0
   div.innerHTML = array
     .map((element, index) => {
       let n = Number(element.id) - 1;
-      totalNumberOfProductsInCart += cartCountArray[n];
-      total = total + cartCountArray[n] * arr[n].price;
-      //   console.log(cartCountArray[n]);
-      //   console.log(arr[n].price);
+      total+=element.count*element.price
+        totalNumberOfProductsInCart+=element.count
+    //   total = total + cartCountArray[n] * arr[n].price;
+    //   console.log(cartCountArray[n]);
+    //   console.log(arr[n].price);
       //   for(let i=0;i<30;i++){
       //     total=total+(cartCountArray[i]*arr[n].price)
 
       //   }
       //   console.log(total)
       // console.log(element.price)
-
+      element.index=index
+      // console.log(element)
       return `<div  class=" productCard p-4 border rounded-lg shadow-md h-fit w-80 flex flex-col justify-center items-center">
               <img src="${element.thumbnail}" alt="${element.title}" class="w-40 h-40 object-cover">
               <h2 class="text-lg font-semibold">${element.title}</h2>
               <p class="text-gray-600">Brand: ${element.brand}</p>
               <p class="text-green-500 font-bold">$${element.price}</p>
                 <div class="flex space-x-1">
-              <button  index=${index} onclick="decrement(${n})" class=" plus  mt-2 px-4 py-2 hover:bg-red-500 bg-red-400 text-white rounded">-</button>
-              <span class="mt-2 px-4 py-2" id="count">${cartCountArray[n]}</span>
-            <button index=${index} onclick="increment(${n})" class=" minus mt-2 px-4 py-2 hover:bg-blue-500 bg-blue-400 text-white rounded">+</button>
+              <button  index=${index} onclick="decrement(${element.index})" class=" plus  mt-2 px-4 py-2 hover:bg-red-500 bg-red-400 text-white rounded">-</button>
+              <span class="mt-2 px-4 py-2" id="count">${element.count}</span>
+            <button index=${index} onclick="increment(${element.index})" class=" minus mt-2 px-4 py-2 hover:bg-blue-500 bg-blue-400 text-white rounded">+</button>
 
             </div>
           </div>`;
@@ -149,51 +151,56 @@ function displayCartData(array = cartArray) {
     .join("");
 
   document.getElementById("bill").innerText = total.toFixed("2");
-  document.getElementById(
-    "checkOut"
-  ).innerText = `Check Out(${totalNumberOfProductsInCart})`;
+  document.getElementById('checkOut').innerText = `Check Out(${totalNumberOfProductsInCart})`
 }
 // let counter=document.getElementById("count")
 //       let count=0;
 function decrement(n) {
+  if(cartArray[n].count>1){
+    cartArray[n].count--
+  }else if(cartArray[n].count==1){
+    cartArray.splice(n,1)
+  }
+   
+    // console.log('jdhdjsks')
   // console.log(index)
-  if (cartCountArray[n] > 1) {
-    cartCountArray[n]--;
-    console.log(n);
-    localStorage.setItem("cartCountArray", JSON.stringify(cartCountArray));
+//   if (cartCountArray[n] > 1) {
+//     cartCountArray[n]--;
+//     console.log(n)
+//     localStorage.setItem("cartCountArray", JSON.stringify(cartCountArray));
 
     // let p=arr[n+1].price
     // console.log(total)
     // total-=p
     // console.log(total)
     // console.log(arr[n+1].price)
-    displayCartData();
-  } else if (cartCountArray[n] == 1) {
+    // displayCartData();
+//   } else if (cartCountArray[n] == 1) {
     // console.log(n + 1);
     // console.log(n)
 
-    cartCountArray[n]--;
-    localStorage.setItem("cartCountArray", JSON.stringify(cartCountArray));
+    // cartCountArray[n]--;
+    // localStorage.setItem("cartCountArray", JSON.stringify(cartCountArray));
 
     // cartArray.splice(n+1,1)
-    cartArray.forEach((elem, index) => {
-      if (elem.id === n + 1) {
-        // let index=getAttribute('index')
-        console.log(elem);
-        cartArray.splice(index, 1);
-        localStorage.setItem("cartArray", JSON.stringify(cartArray));
-      }
-    });
+    // cartArray.forEach((elem,index) => {
+    //   if (elem.id === n+1 ) {
+    //     // let index=getAttribute('index')
+    //     console.log(elem)
+    //     cartArray.splice(index, 1);
+    //     localStorage.setItem("cartArray", JSON.stringify(cartArray));
+    //   }
+    // });
 
     displayCartData();
-  }
+//   }
 
   // console.log(cartCountArray[n])
 }
 function increment(n) {
-  //
-  cartCountArray[n]++;
-  localStorage.setItem("cartCountArray", JSON.stringify(cartCountArray));
+    cartArray[n].count++
+//   cartCountArray[n]++;
+//   localStorage.setItem("cartCountArray", JSON.stringify(cartCountArray));
 
   // total+=arr[n+1].price
   // console.log(arr[n+1].price)
@@ -208,39 +215,30 @@ function increment(n) {
 
 // let checkOut=document.getElementById('checkOut')
 // checkOut.addEventListener('click',()=>{
-function checkOut() {
-  let a = document.getElementById("co");
-  a.innerHTML = "Check Out";
-  let checkOutForm = document.getElementById("checkOutForm");
-  checkOutForm.classList.remove("hidden");
-}
+    function checkOut(){
+    let a=document.getElementById('co')
+    a.innerHTML='Check Out'
+    let checkOutForm=document.getElementById('checkOutForm')
+    checkOutForm.classList.remove('hidden')
+    }
 // })
-function confirmPayment() {
-  let a = document.getElementById("co");
-  a.innerHTML = "Order Summary";
-  let c = document.getElementById("orderSummary");
-  c.innerHTML = "";
+function confirmPayment(){
+let a=document.getElementById('co')
+a.innerHTML='Order Summary'
+let c = document.getElementById('orderSummary')
+c.innerHTML=''
 
-  c.innerHTML = cartArray
-    .map(
-      (
-        element
-      ) => `<div class="flex justify-between"><h2 class="text-lg font-semibold">${
-        element.title
-      }</h2>
+
+c.innerHTML=cartArray.map(element=>`<div class="flex justify-between"><h2 class="text-lg font-semibold">${element.title}</h2>
                 
-                <p class="text-green-500 font-bold">$${
-                  element.price * cartCountArray[Number(element.id) - 1]
-                }</p>
+                <p class="text-green-500 font-bold">$${(element.price)*element.count}</p>
                 </div>
-    `
-    )
-    .join("");
-  let t = document.getElementById("thank");
-  t.classList.remove("hidden");
-  // window.onclick=function(){
-  // let a = document.getElementById("checkOutForm")
-  // a.classList.add('hidden')
+    `).join('')
+    let t=document.getElementById('thank')
+    t.classList.remove('hidden')
+    // window.onclick=function(){
+    // let a = document.getElementById("checkOutForm")
+    // a.classList.add('hidden')
 
-  // }
+    // }
 }
